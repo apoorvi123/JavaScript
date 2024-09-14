bc7fe3d496f40f069be616af5e3bbd0b6595a6d4
Apoorvi Singh <aporvasingh229@gmail.com>
2024-09-14 08:58:25 -0700

    DAY 1

---
+++ Hello.js
@@ -0,0 +1,5 @@
+console.log("Hello World!!!");
+console.log("Apoorvi Singh");
+console.log("Gaurav sir is my teacher");
+console.log("Anurag Singh");
+console.log("Gaurav Verma");
---
+++ Variables.js
@@ -0,0 +1,8 @@
+let x = 100;
+console.log(x);
+
+const y = 200;
+console.log(y);
+
+var z = 500;
+console.log(z);
