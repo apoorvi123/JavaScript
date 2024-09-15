8827ab5907e0c7c8b254b8d374e5ed4b644c6534
Apoorvi Singh <aporvasingh229@gmail.com>
2024-09-15 08:46:18 -0700

    Day 2

---
+++ Day 2.js
@@ -0,0 +1,11 @@
+// Playing With Variables
+
+
+let productName = "Iphone13 pro max";
+const productPrice = 25000;
+var companyName = "Apple"
+
+// console.log (productName, productPrice, companyName);
+console.log ("Name of the Company :" ,companyName );
+console.log ("Price of the Procuct:",productPrice);
+console.log ("Name of the Company:",companyName);
---
+++ stringDemo.js
@@ -0,0 +1,11 @@
+let singleQuoteString = 'Apoorvi';
+let doubleQuoteString = "Anurag";
+let backTickeString = `Gaurav`;
+console.log (singleQuoteString,doubleQuoteString,backTickeString);
+
+// Backticke are used for multilining
+
+let paragraph = `My name is Apoorvi.
+I pursing my Btech from srms.
+I am skilled in HTML,CSS asnd J.S.`;
+console.log (paragraph);
